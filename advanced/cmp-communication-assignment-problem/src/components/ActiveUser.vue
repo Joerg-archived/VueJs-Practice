2e57e9aca9508d4f837c90d6d1d981e14dd588e7
Jörg Dillmann <>
2022-11-04 16:45:44 +0100

    finished props & custom events assignment

---
+++ advanced/cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>Username: {{ name }}</h2>
+  <h3>Age: {{ age }}</h3>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
