2123fb88962c8ccbf53e5e590de1a445c85f2345
Jörg Dillmann <>
2022-11-04 10:52:35 +0100

    added friendcontact component and added it to App.vue

---
+++ vue-cli/vue-cli-example/src/components/FriendContact.vue
@@ -0,0 +1,30 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleButton">{{ detailsAreVisible ? "Hide" : "Show" }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: true,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "01234 5678 991",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleButton() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
