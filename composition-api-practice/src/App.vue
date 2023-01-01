2d42b1c77f2f9845879538d4b3e28e7c88556f9a
ReconSoul <dillmann.joerg@outlook.de>
2023-01-01 21:00:33 +0100

    migration project management app so it uses composition api

--- composition-api-practice/src/App.vue
+++ composition-api-practice/src/App.vue
@@ -1,3 +1,4 @@
+<!-- Setup replaces beforeCreate and created lifecycle hooks -->
 <script setup>
 /* Using script setup has advantages regarding the setup method variant:
  *  + More succinct code with less boilerplate
@@ -7,7 +8,9 @@
  */
 import { ref, computed, watch } from "vue";
 // reactive is only for objects, ref for everyone
-import { reactive, toRefs } from "vue";
+import { reactive, toRefs, provide } from "vue";
+// lifecycle imports
+import { onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from "vue";
 // const userName = ref("Maximilian");
 // const age = ref(30);
 
@@ -16,15 +19,19 @@ const name = reactive({
   lastName: "",
 });
 
+const lastNameInput = ref(null);
+
 const user = reactive({
   // computed properties / refs are read only, i.e. you can't modify them with x.value = newValue
-  userName: computed(function () {
-    return name.firstName + " " + name.lastName;
-  }),
+  // userName: computed(function () {
+  //   return name.firstName + " " + name.lastName;
+  // }),
   age: 30,
 });
+
 // Expose nested attributes as reactive
 const { userName, age } = toRefs(user);
+provide("userAge", age);
 
 // setTimeout(function () {
 //   // only user.value is reactive using "ref", to omit this we also can use "reactive" instead of "ref"
@@ -49,22 +56,40 @@ watch(age, function (newValue, oldValue) {
 // function setLastName(event) {
 //   name.lastName = event.target.value;
 // }
+
+function setLastName() {
+  // First value is a reference to the input name declared with "ref=lastNameInput"
+  name.lastName = lastNameInput.value.value;
+}
+
+onBeforeMount(function () {
+  console.log("before mount");
+});
+onBeforeUpdate(function () {
+  console.log("before update");
+})
+onUpdated(function() {
+  console.log("on update");
+})
 </script>
 
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
-    <h3>{{ age }}</h3>
+    <user-data :firstName="name.firstName" :lastName="name.lastName" />
+
     <button @click="incrementAge">Increment age</button>
     <div>
       <input type="text" v-model="name.firstName" placeholder="First Name" />
-      <input type="text" v-model="name.lastName" placeholder="Last Name" />
+      <input type="text" ref="lastNameInput" placeholder="Last Name" />
+      <button @click="setLastName">Set Last Name</button>
     </div>
   </section>
 </template>
 
 <script>
+import UserData from "./components/UserData.vue";
 export default {
+  components: [UserData],
   setup() {
     // Only values created with "ref" will be reactive
     // const uName = ref("Maximilian");
---
+++ composition-api-practice/src/components/UserData.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed, provide, inject} from "vue";
+
+const emits = defineEmits();
+console.log(emits);
+
+const props = defineProps(["firstName", "lastName"]);
+
+const age = inject("userAge");
+const userName = computed(function () {
+  return props.firstName + " " + props.lastName;
+});
+</script>
+
+<template>
+  <h2>{{ userName }}</h2>
+  <h3>{{ age }}</h3>
+</template>
+
+<script>
+export default {
+  // props: ["firstName", "lastName", "age"],
+};
+</script>
